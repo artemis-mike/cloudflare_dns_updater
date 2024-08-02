85c31971a7d8f13c3b8e688b78f9446a18f7852b
Michael Schmid <michael-schmid@posteo.de>
2024-08-02 23:00:59 +0200

    Correct kill syntax

--- source/health.sh
+++ source/health.sh
@@ -5,5 +5,5 @@ export $(cat .env | xargs)
 if [ "$(expr $(date +%s) - $(cat lastRun.epoch))" -lt "$(expr $CF_UPDATER_INTERVAL \* 2)" ]; then 
     exit 0; 
 else
-    kill -15 1;
+    kill -s 15 1;
 fi
