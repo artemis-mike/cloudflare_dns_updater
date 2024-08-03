1654c78a4d6e3d6c4f8b995382b8bd893947af06
Michael Schmid <michael-schmid@posteo.de>
2024-08-03 14:35:14 +0200

    Remove signal.SIGKILL as it can't be caught

--- source/update-a-record.py
+++ source/update-a-record.py
@@ -111,6 +111,7 @@ def main():
 
 if __name__ == "__main__":
   signal.signal(signal.SIGINT, signal_handler)
-  signal.signal(signal.SIGKILL, signal_handler)
   signal.signal(signal.SIGHUP, signal_handler)
+  signal.signal(signal.SIGTERM, signal_handler)
+
   main()
